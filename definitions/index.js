9c84437fa9f14c164d269878e6fd6108eb33774e
Sam Murray <samueljmurray@gmail.com>
2017-03-09 09:53:23 +0000

    don't use export * from '...' syntax because of incompatibility with babel-plugin-rewire

--- definitions/index.js
+++ definitions/index.js
@@ -1,11 +1,181 @@
-export * from './attention-seekers';
-export * from './bouncing-entrances';
-export * from './bouncing-exits';
-export * from './fading-entrances';
-export * from './fading-exits';
-export * from './flippers';
-export * from './lightspeed';
-export * from './sliding-entrances';
-export * from './sliding-exits';
-export * from './zooming-entrances';
-export * from './zooming-exits';
+// Export method modified because babel-plugin-rewire is incompatible with `export * from '...'` syntax
+// See: https://github.com/speedskater/babel-plugin-rewire/issues/78
+
+import {
+  bounce,
+  flash,
+  jello,
+  pulse,
+  rotate,
+  shake,
+  swing,
+  rubberBand,
+  tada,
+  wobble
+} from './attention-seekers';
+export {
+  bounce,
+  flash,
+  jello,
+  pulse,
+  rotate,
+  shake,
+  swing,
+  rubberBand,
+  tada,
+  wobble
+};
+
+import {
+  bounceIn,
+  bounceInUp,
+  bounceInDown,
+  bounceInRight,
+  bounceInLeft
+} from './bouncing-entrances';
+export {
+  bounceIn,
+  bounceInUp,
+  bounceInDown,
+  bounceInRight,
+  bounceInLeft
+};
+
+import {
+  bounceOut,
+  bounceOutUp,
+  bounceOutDown,
+  bounceOutRight,
+  bounceOutLeft
+} from './bouncing-exits';
+export {
+  bounceOut,
+  bounceOutUp,
+  bounceOutDown,
+  bounceOutRight,
+  bounceOutLeft
+};
+
+import {
+  fadeIn,
+  fadeInDown,
+  fadeInUp,
+  fadeInLeft,
+  fadeInRight,
+  fadeInDownBig,
+  fadeInUpBig,
+  fadeInLeftBig,
+  fadeInRightBig
+} from './fading-entrances';
+export {
+  fadeIn,
+  fadeInDown,
+  fadeInUp,
+  fadeInLeft,
+  fadeInRight,
+  fadeInDownBig,
+  fadeInUpBig,
+  fadeInLeftBig,
+  fadeInRightBig
+};
+
+import {
+  fadeOut,
+  fadeOutDown,
+  fadeOutUp,
+  fadeOutLeft,
+  fadeOutRight,
+  fadeOutDownBig,
+  fadeOutUpBig,
+  fadeOutLeftBig,
+  fadeOutRightBig
+} from './fading-exits';
+export {
+  fadeOut,
+  fadeOutDown,
+  fadeOutUp,
+  fadeOutLeft,
+  fadeOutRight,
+  fadeOutDownBig,
+  fadeOutUpBig,
+  fadeOutLeftBig,
+  fadeOutRightBig
+};
+
+import {
+  flipInX,
+  flipInY,
+  flipOutX,
+  flipOutY
+} from './flippers';
+export {
+  flipInX,
+  flipInY,
+  flipOutX,
+  flipOutY
+};
+
+import {
+  lightSpeedIn,
+  lightSpeedOut
+} from './lightspeed';
+export {
+  lightSpeedIn,
+  lightSpeedOut
+};
+
+import {
+  slideInDown,
+  slideInUp,
+  slideInLeft,
+  slideInRight
+} from './sliding-entrances';
+export {
+  slideInDown,
+  slideInUp,
+  slideInLeft,
+  slideInRight
+};
+
+import {
+  slideOutDown,
+  slideOutUp,
+  slideOutLeft,
+  slideOutRight
+} from './sliding-exits';
+export {
+  slideOutDown,
+  slideOutUp,
+  slideOutLeft,
+  slideOutRight
+};
+
+import {
+  zoomIn,
+  zoomInDown,
+  zoomInUp,
+  zoomInLeft,
+  zoomInRight
+} from './zooming-entrances';
+export {
+  zoomIn,
+  zoomInDown,
+  zoomInUp,
+  zoomInLeft,
+  zoomInRight
+};
+
+import {
+  zoomOut,
+  zoomOutDown,
+  zoomOutUp,
+  zoomOutLeft,
+  zoomOutRight
+} from './zooming-exits';
+export {
+  zoomOut,
+  zoomOutDown,
+  zoomOutUp,
+  zoomOutLeft,
+  zoomOutRight
+};
